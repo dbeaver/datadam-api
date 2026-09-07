2039b8aa5065591d0d4e608cd51066d8e3e49356
Solovev Anton <solovev.anton93@gmail.com>
2026-09-07 18:54:32 +0200

    dbeaver/pro#10606: add api v0.01

---
+++ apis/com.dbeaver.datadam.share.api/src/main/java/com/dbeaver/datadam/share/api/service/DDProjectService.java
@@ -0,0 +1,46 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2026 DBeaver Corp
+ *
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of DBeaver Corp and its suppliers, if any.
+ * The intellectual and technical concepts contained
+ * herein are proprietary to DBeaver Corp and its suppliers
+ * and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from DBeaver Corp.
+ */
+package com.dbeaver.datadam.share.api.service;
+
+import com.dbeaver.datadam.share.api.model.DDProject;
+import com.dbeaver.datadam.share.api.model.DDProjectConfiguration;
+import org.jkiss.code.NotNull;
+import org.jkiss.code.Nullable;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface DDProjectService {
+
+    @NotNull
+    List<DDProject> listProjects() throws DDShareException;
+
+    @NotNull
+    DDProject createProject(@NotNull String name, @Nullable String description) throws DDShareException;
+
+    @NotNull
+    DDProject updateProject(@NotNull UUID projectId, @NotNull String name, @Nullable String description)
+        throws DDShareException;
+
+    boolean deleteProject(@NotNull UUID projectId) throws DDShareException;
+
+    @NotNull
+    DDProjectConfiguration pullProjectConfiguration(@NotNull UUID projectId) throws DDShareException;
+
+    boolean pushProjectConfiguration(@NotNull UUID projectId, @NotNull DDProjectConfiguration configuration)
+        throws DDShareException;
+}
