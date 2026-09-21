8d83eea976ded4172d52685da9adf2980779795e
Solovev Anton <121192850+SolovevAnton@users.noreply.github.com>
2026-09-21 16:33:49 +0200

    Dbeaver/pro#10566 datadam individual project support (#7)
    * dbeaver/pro#10566: add cuurent revision
    
    * dbeaver/pro#10566: removed configuration from pull
    
    * dbeaver/pro#10566: refactor to revision not null
    
    * dbeaver/pro#10566: add ProjectSnapshot

--- apis/com.dbeaver.datadam.share.api/src/main/java/com/dbeaver/datadam/share/api/service/DDSharedProjectService.java
+++ apis/com.dbeaver.datadam.share.api/src/main/java/com/dbeaver/datadam/share/api/service/DDSharedProjectService.java
@@ -19,6 +19,7 @@ package com.dbeaver.datadam.share.api.service;
 import com.dbeaver.datadam.share.api.exception.DDShareException;
 import com.dbeaver.datadam.share.api.model.DDSharedProject;
 import com.dbeaver.datadam.share.api.model.DDSharedProjectConfiguration;
+import com.dbeaver.datadam.share.api.model.DDSharedProjectPullResponse;
 import com.dbeaver.datadam.share.api.model.DDSharedProjectRevision;
 import org.jkiss.code.NotNull;
 import org.jkiss.code.Nullable;
@@ -52,7 +53,10 @@ public interface DDSharedProjectService {
     boolean deleteProject(@NotNull UUID projectId) throws DDShareException;
 
     @NotNull
-    DDSharedProjectConfiguration pullProjectConfiguration(@NotNull UUID projectId) throws DDShareException;
+    DDSharedProjectPullResponse pullProjectConfiguration(@NotNull UUID projectId) throws DDShareException;
+
+    @NotNull
+    DDSharedProjectRevision getCurrentProjectRevision(@NotNull UUID projectId) throws DDShareException;
 
     /**
      * Pushes project content if {@code lastKnownRevision} matches the current server revision.
